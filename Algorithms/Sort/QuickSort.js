8face831711435e4456b293cb1006203c110121d
Chao Zhang <czhang@axonify.com>
2020-05-04 22:53:14 -0400

    Add Quick Sort

---
+++ Algorithms/Sort/QuickSort.js
@@ -0,0 +1,31 @@
+function quickSort(array) {
+  function sort(p, r) {
+    if (p >= r) {
+      return;
+    }
+    
+    const q = partition(p, r);
+    sort(p, q - 1);
+    sort(q + 1, r);
+  }
+
+  function partition(p, r) {
+    let pivot = array[r];
+    let i = p;
+    for (let j = p; j < r; j++) {
+      if (array[j] < pivot) {
+        [array[i], array[j]] = [array[j], array[i]];
+        i++;
+      }
+    }
+
+    [array[i], array[r]] = [array[r], array[i]];
+    return i;
+  }
+
+  sort(0, array.length - 1);
+}
+
+// time:  O(n^2)
+// space: O(1) - in place
+// not stable
