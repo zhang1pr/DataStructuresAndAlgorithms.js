53a9b3e6dae8bcee80983be3c594a80dfac12858
Chao Zhang <czhang@axonify.com>
2020-05-31 23:38:08 -0400

    Add Permutations

---
+++ Algorithms/Permutations.js
@@ -0,0 +1,24 @@
+function permutations(array) {
+  const result = [];
+
+  function permute(array, k) {
+    if (k == 1) {
+      result.push(array.slice());
+    }
+  
+    for (let i = 0; i < k; i++) {
+      [array[i], array[k - 1]] = [array[k - 1], array[i]]
+  
+      permute(array, k - 1);
+      
+      [array[i], array[k - 1]] = [array[k - 1], array[i]]
+    }
+  }
+
+  permute(array, array.length);
+
+  return result;
+}
+
+// time:  O(n!)
+// space: O(1)
