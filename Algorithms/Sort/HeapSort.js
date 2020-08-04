f771ff40e23deb77ce1e27d420f51d3056f306ec
Chao Zhang <czhang@axonify.com>
2020-08-03 21:30:15 -0400

    Change Heap Sort

--- Algorithms/Sort/HeapSort.js
+++ Algorithms/Sort/HeapSort.js
@@ -1,19 +1,9 @@
-const Heap = require('../../DataStructures/Heap');
+const StaticHeap = require('../../DataStructures/Heap.js');
 
 function HeapSort(array) {
-  const heap = new Heap()
-  
-  while (array.length) {
-    heap.add(array.pop());
-  }
-  
-  while (!heap.isEmpty()) {
-    array.push(heap.poll());
-  }
-
-  return array;
+  return new StaticHeap(array).sort();
 }
 
 // time:  O(nlog(n))
-// space: O(1)
+// space: O(1) - in place
 // not stable
