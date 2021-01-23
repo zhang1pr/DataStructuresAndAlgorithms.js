746f9da05ad18224d14d1e286316e0b92ab02a53
Chao Zhang <czhang@axonify.com>
2021-01-23 17:35:32 -0500

    Change Stack By Array

--- DataStructures/StackByArray.js
+++ DataStructures/StackByArray.js
@@ -1,13 +1,27 @@
 class StackByArray {
-  constructor() {
-    
+  constructor(capacity) {
+    this.items = Array(capacity);
+    this.capacity = capacity;
+    this.count = 0;
   }
 
   push(item) {
-   
+    if (this.count == this.capacity) {
+      return false;
+    }
+
+    this.items[this.count] = item;
+    this.count++;
+    return true;
   }
 
   pop() {
-    
+    if (this.count == 0) {
+      return null;
+    }
+
+    const result = this.items[this.count - 1];
+    this.count--;
+    return result;
   }
 }
--- DataStructures/StackByArrayTemplate.js
+++ DataStructures/StackByArrayTemplate.js
@@ -1,27 +1,13 @@
 class StackByArray {
-  constructor(capacity) {
-    this.items = [...new Array(capacity)];
-    this.capacity = capacity;
-    this.count = 0;
+  constructor() {
+    
   }
 
   push(item) {
-    if (this.count == this.capacity) {
-      return false;
-    }
-
-    this.items[this.count] = item;
-    this.count++;
-    return true;
+   
   }
 
   pop() {
-    if (this.count == 0) {
-      return null;
-    }
-
-    const result = this.items[this.count - 1];
-    this.count--;
-    return result;
+    
   }
 }
